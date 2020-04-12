9823c8d77bfc5146e6d21d84223332f1bb4701ed
Devashish Rajput <42275894+Devashish-Rajput@users.noreply.github.com>
2020-04-12 19:41:41 +0530

    My First Android and Ios App
    The project is based on flutter..

---
+++ pixelchat/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:buddiesgram/pages/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PixelChat',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData
+      (
+        scaffoldBackgroundColor: Colors.black,
+        dialogBackgroundColor: Colors.black,
+        primarySwatch: Colors.grey,
+          accentColor: Colors.black,
+        cardColor: Colors.white70
+      ),
+      home: HomePage(),
+    );
+  }
+}
